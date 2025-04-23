5b8d0aa8456efff96da2145cca74befadf4b3870
Денис Котляров <denis2005991@gmail.com>
2025-04-23 23:41:25 +0300

    feat: rework example

--- examples/for_match.rs
+++ examples/for_match.rs
@@ -1,17 +1,20 @@
 use pattern_loops::for_match;
 
-fn main() {
-	let data = b"123456789";
+/*
+	A classic example of using parsing a number from an arbitrary
+	ASCII character set with partial trimming support.
+*/
 
-	let num = for_match!((data, _, 0usize) -> |_, num| {
-		Some(b'0') => {},
-		Some(a @ b'1' ..= b'9') => {
-			num *= 10;
-			num += (a - b'0') as usize;
-		},
-		Some(a) => panic!("Unk byte: {:?}", a),
-		_ => break num,
+fn main() -> Result<(), String> {
+	let data = b" 1234567890 ";
+
+	let out = for_match!((data, _, 0usize) -> |_, num| {
+		Some(b' ') => {},
+		Some(a @ b'0' ..= b'9') => num = num * 10 + ((a - b'0') as usize),
+		Some(a) => return Err(format!("Unknown byte: {:?}", a)),
+		None => break num,
 	});
 
-	println!("{}", num);
+	assert_eq!(out, 1234567890);
+	Ok(())
 }
