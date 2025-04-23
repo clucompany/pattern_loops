04dc0a69fd72d7ad2f9ef629fd4e32c5c80b58fd
Денис Котляров <denis2005991@gmail.com>
2025-04-23 23:42:13 +0300

    feat and doc: rework the example base to make it clearer and comment parts of the code

--- examples/read_file.rs
+++ examples/read_file.rs
@@ -1,39 +1,57 @@
 use std::{fs::File, io::Read};
 use pattern_loops::{for_match, while_match};
+use std::{
+	fs::File,
+	io::{Error, Read},
+};
 
-fn main() -> Result<(), std::io::Error> {
+/*
+	Example of block-by-block reading of a file consisting of many lines,
+	with support for comments and the exclusion of unnecessary control characters
+*/
+
+fn main() -> Result<(), Error> {
 	let mut read_buffer = [0u8; 128];
 	let mut buffer = Vec::with_capacity(130);
 	let mut file = File::open("./examples/read_file.txt")?;
 
-	while_match!((file.read(&mut read_buffer)) -> || {
-		Ok(0) => break,
-		Ok(len) => {
-			let real_array = &read_buffer[..len];
-
-			for_match!(@'read (real_array.iter()) -> |iter| {
-				Some(13u8) => continue,
-				Some(b'\n') => {
-					if !buffer.is_empty() {
-						println!("#line: {}", String::from_utf8_lossy(&buffer));
-						buffer.clear();
-					}
-				},
-				Some(b'#') => while_match!((iter) -> || {
-					Some(b'\n') => continue 'read,
-					Some(_a) => {},
-					_ => break 'read,
-				}),
-				Some(a) => buffer.push(*a),
-				_ => break,
-			});
-		},
+	while_match!(@'next_readfile (file.read(&mut read_buffer)) -> || {
+		Ok(0) => break 'next_readfile, // end
+		Ok(len) => for_match!(@'read_cycle (read_buffer[..len].iter()) -> |iter| {
+			// symbols that are absolutely not interesting,
+			// but should not be included in the line
+			Some(b'\r') => continue 'read_cycle,
+			// new line
+			Some(b'\n') if !buffer.is_empty() => show_line_and_clear_buffer(&mut buffer),
+			Some(b'\n') => {},
+			// implements support for unix comments in a file that do
+			// not affect the data in any way
+			Some(b'#') => while_match!((iter) -> || {
+				Some(b'\n') => continue 'read_cycle,
+				Some(..) => {},
+				// if we run out of data, we try to read the file further.
+				// if there is enough data, the cycle continues
+				None => match file.read(&mut read_buffer) {
+					Ok(0) => break 'next_readfile, // end
+					Ok(len) => iter = read_buffer[..len].iter(),
+					Err(e) => return Err(e),
+				}
+			}),
+			// write useful data
+			Some(a) => buffer.push(*a),
+			_ => break,
+		}),
 
 		Err(e) => return Err(e),
 	});
 	if !buffer.is_empty() {
-		println!("#line: {}", String::from_utf8_lossy(&buffer));
+		show_line_and_clear_buffer(&mut buffer);
 	}
 
 	Ok(())
+}
+
+pub fn show_line_and_clear_buffer(buff: &mut Vec<u8>) {
+	println!("#line: {}", String::from_utf8_lossy(buff));
+	buff.clear();
 }
